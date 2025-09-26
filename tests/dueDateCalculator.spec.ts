5920dd5fa0d12a069f6691d35f80bb95348c86e0
Addessi Sameh <145492561+samehsadine@users.noreply.github.com>
2025-09-26 16:15:23 -0400

    tests: Add unit tests for dueDateCalculator scenarios

--- tests/dueDateCalculator.spec.ts
+++ tests/dueDateCalculator.spec.ts
@@ -1,6 +1,44 @@
 import { calculateDueDate } from "../src/dueDateCalculator";
 
-describe ("", () => {
+function getDate(year: number, month: number, day: number, hours: number, minutes: number = 0): Date {
+        return new Date(year, month -1 , day, hours, minutes, 0, 0);
+    }
+describe ("Due Date Calculator - Working Hours 9:00 to 17:00 (Monday->Friday)", () => {
 
- 
+    test("Submit on Monday 9:00 + 8 hours = Monday 17:00", () => {
+        const submit = getDate(2025, 9, 22, 9,0);
+        const result = calculateDueDate(submit, 8);
+        expect(result).toEqual(getDate(2025, 9, 22, 17,0));
+        
+    });
+
+    test("Submit on Tuesday 14:00 + 4 hours = Wednesday 10:00", () => {
+        const submit = getDate(2025, 9, 23, 14,0);
+        const result = calculateDueDate(submit, 4);
+        expect(result).toEqual(getDate(2025, 9, 24, 10,0));
+        
+    });
+
+    test("Submit on Friday 15:00 + 4 hours = Monday 11:00", () => {
+        const submit = getDate(2025, 9, 26, 15,0);
+        const result = calculateDueDate(submit, 4);
+        expect(result).toEqual(getDate(2025, 9, 29, 11,0));
+        
+    });
+
+
+    test("Submit on Tuesday 14:12 + 16 hours = Thursday 14:12", () => {
+        const submit = getDate(2025, 9, 23, 14, 12);
+        const result = calculateDueDate(submit, 16);
+        expect(result).toEqual(getDate(2025, 9, 25, 14,12));
+        
+    });
+
+    test("Submit on Tuesday 11:00 + 40 hours = Next Tuesday 11:00", () => {
+        const submit = getDate(2025, 9, 23, 11, 0);
+        const result = calculateDueDate(submit, 40);
+        expect(result).toEqual(getDate(2025, 9, 30, 11,0));
+        
+    });
+
 });
